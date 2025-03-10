f24cada2afe3ce3604603e535f96e9cf6f090873
AndresRioC <183239152+AndresRioC@users.noreply.github.com>
2025-03-10 17:30:55 +0100

    Latest working version

--- apps/client/src/routes/weather-api.ts
+++ apps/client/src/routes/weather-api.ts
@@ -1,20 +1,22 @@
-import dotenv from 'dotenv'
-dotenv.config()
+const OPENWEATHER_BASE_URL = import.meta.env.VITE_OPENWEATHER_BASE_URL;
+const OPENWEATHER_TOKEN = import.meta.env.VITE_OPENWEATHER_TOKEN;
+const OIKOLAB_BASE_URL = import.meta.env.VITE_OIKOLAB_BASE_URL;
+const OIKOLAB_TOKEN = import.meta.env.VITE_OIKOLAB_TOKEN;
 
 export async function getCurrentWeather(cityname: string) {
   const weatherResponse = await fetch(
-    `${process.env.OPENWEATHER_BASE_URL}weather?q=${cityname}&appid=${process.env.OPENWEATHER_TOKEN}`
-  )
-  const weatherData = await weatherResponse.json()
-  console.log(weatherData)
+    `${OPENWEATHER_BASE_URL}weather?q=${cityname}&appid=${OPENWEATHER_TOKEN}`
+  );
+  const weatherData = await weatherResponse.json();
+  console.log(weatherData);
   const airResponse = await fetch(
-    `${process.env.OPENWEATHER_BASE_URL}air_pollution?lat=${weatherData.corod.lat}&lon=${weatherData.corod.lon}&appid=${process.env.OPENWEATHER_TOKEN}`
-  )
-  const airData = await airResponse.json()
-  console.log(airData)
-  const data: object = { ...weatherData, ...airData }
-  console.log(data)
-  return data
+    `${OPENWEATHER_BASE_URL}air_pollution?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=${OPENWEATHER_TOKEN}`
+  );
+  const airData = await airResponse.json();
+  console.log(airData);
+  const data: object = { ...weatherData, ...airData };
+  console.log(data);
+  return data;
 }
 
 export async function getWeatherHistory(
@@ -25,9 +27,9 @@ export async function getWeatherHistory(
   end: string
 ) {
   const historyResponse = await fetch(
-    `${process.env.OIKOLAB_BASE_URL}?param=${type}&lat=${lat}&lon=${lon}&start=${start}&end=${end}&api-key=${process.env.OIKOLAB_TOKEN}`
-  )
-  const historyData = await historyResponse.json()
-  console.log(historyData)
-  return historyData
+    `${OIKOLAB_BASE_URL}?param=${type}&lat=${lat}&lon=${lon}&start=${start}&end=${end}&api-key=${OIKOLAB_TOKEN}`
+  );
+  const historyData = await historyResponse.json();
+  console.log(historyData);
+  return historyData;
 }
